b7ceabd864dc38b1dc5458f0eb9f128e4ff50045
Nick Navarro <jacinto802@gmail.com>
2018-01-11 18:08:26 -0500

    Routing for profile view now retrieving user profile, user's recipes, and populate on user.

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -13,20 +13,45 @@ const myUploader = multer({
 
 
 
+// router.get('/api/profile/:id', (req, res, next) => {
+//   RecipeModel
+//   .find({user: req.params.id}, (err, theRecipe) => {
+//     if(err) {return next(err); }
+//   })
+//   .populate('user', {encryptedPassword : 0})
+//   .exec((err, theRecipe) => {
+//     if(err) {
+//       res.status(500).json({ message: 'Could not find the recipe'});
+//       return;
+//     }
+//     console.log(theRecipe);
+//     res.status(200).json(theRecipe)
+//   })
+// })
+
 router.get('/api/profile/:id', (req, res, next) => {
-  RecipeModel
-  .find({user: req.params.id}, (err, theRecipe) => {
-    if(err) {return next(err); }
-  })
-  .populate('user', {encryptedPassword : 0})
-  .exec((err, theRecipe) => {
-    if(err) {
-      res.status(500).json({ message: 'Could not find the recipe'});
-      return;
-    }
-    res.status(200).json(theRecipe)
-  })
-})
+  UserModel
+  .findById(req.params.id, (err, theUser) => {
+    if(err) {return next(err);}
+    RecipeModel
+    .find({user: req.params.id}, (err, theRecipe) =>{
+      if(err) {return next(err); }
+    })
+    .populate('user', {encryptedPassword : 0})
+    .exec((err, theRecipe) => {
+      if(err) {
+        res.status(500).json({ message: 'Could not find the recipe'});
+        return;
+      }
+      const data ={
+        user: theUser,
+        recipe: theRecipe
+      }
+      console.log(data);
+      res.status(200).json(data)
+    })
+  })//closer UserModel.findById
+}) //close router.get
 
 
 
